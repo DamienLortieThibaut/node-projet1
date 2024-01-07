9e4c8012104021310631fac248a5a6033b2cb390
Damien <dlortiethibaut@gmail.com>
2024-01-07 23:02:21 +0100

    front details caroption

--- front/src/pages/Custom/Custom.jsx
+++ front/src/pages/Custom/Custom.jsx
@@ -1,40 +1,59 @@
 // Custom.jsx
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import './Custom.css'
-import { modelApi, getApi } from '../../services/api';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./Custom.css";
+import { modelApi, getApi } from "../../services/api";
 
 function Custom() {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [Tools, setTools] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchPrimaryTools = async () => {
+      try {
+        const response = await getApi.getByModelId(id);
+        setTools(response);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des outils obligatoires:",
+          error
+        );
+      }
+    };
 
-    }, [])
-
+    fetchPrimaryTools();
+  }, []);
 
   return (
-    <div className='custom'>
+    <div className="custom">
+      <div>
+        <img />
+      </div>
+      <div>
         <div>
-             <img />
+          <h3>Kit de base</h3>
+          <div>
+            {Tools.map(tool => (
+              tool.is_primary ? (
+                <div key={tool.id}>
+                  <p>Nom: {tool.name}</p>
+                  <p>Prix: { tool.prize}</p>
+                </div>
+              ) : null
+            ))
+
+            }
+          </div>
         </div>
-       <div>
-            <div>
-                <h3>Kit de base</h3>
-                <div>
-                  
-                </div>
-            </div>
-            <div>
-                <h3>Options</h3>
-                <div>
-                    
-                </div>
-            </div>
-            <div>
-                <p>Prize: </p>
-                <button>Acheter</button>
-            </div>
-       </div>
+        <div>
+          <h3>Options</h3>
+          <div></div>
+        </div>
+        <div>
+          <p>Prize: </p>
+          <button>Acheter</button>
+        </div>
+      </div>
     </div>
   );
 }
