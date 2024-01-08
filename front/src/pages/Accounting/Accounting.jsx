eec7c9983a02755b9580f99b998297df9d5c16af
Damien <dlortiethibaut@gmail.com>
2024-01-08 13:21:42 +0100

    add select feateures accounting

--- front/src/pages/Accounting/Accounting.jsx
+++ front/src/pages/Accounting/Accounting.jsx
@@ -7,8 +7,44 @@ import { useAuth } from "../../utils/provider";
 function Accounting() {
   const { accessToken } = useAuth();
   const [buys, setBuys] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
+  const [totalPrize, setTotalPrize] = useState(0);
 
   useEffect(() => {
+    const filteredBuys = buys.filter((buy) => {
+      const buyDate = new Date(buy.date);
+      const buyMonth = buyDate.getMonth() + 1;
+      const buyYear = buyDate.getFullYear();
+      console.log(buyMonth);
+      return (
+        (selectedMonth === "00" ||
+          selectedMonth === "" ||
+          buyMonth === parseInt(selectedMonth, 10)) &&
+        (selectedYear === "00" ||
+          selectedYear === "" ||
+          buyYear === parseInt(selectedYear, 10))
+      );
+    });
+
+    calculateTotalPrize(filteredBuys);
+  }, [buys, selectedMonth, selectedYear]);
+
+  const handleFilterChange = (type, value) => {
+    if (type === "month") {
+      setSelectedMonth(value);
+    } else if (type === "year") {
+      setSelectedYear(value);
+    }
+  };
+
+  const calculateTotalPrize = (buysData) => {
+    const total = buysData.reduce((acc, buy) => acc + buy.prize, 0);
+    setTotalPrize(total);
+  };
+
+  useEffect(() => {
+    // Charger tous les achats au montage initial
     buyApi
       .getAll(accessToken)
       .then((data) => setBuys(data))
@@ -19,14 +55,47 @@ function Accounting() {
 
   return (
     <div className="accounting">
+      <div className="filter">
+        <select
+          value={selectedMonth}
+          onChange={(e) => handleFilterChange("month", e.target.value)}
+        >
+          <option value="00">Tous les mois</option>
+          <option value="01">Janvier</option>
+          <option value="02">Février</option>
+          <option value="03">Mars</option>
+          <option value="04">Avril</option>
+          <option value="05">Mai</option>
+          <option value="06">Juin</option>
+          <option value="07">Juillet</option>
+          <option value="08">Aout</option>
+          <option value="09">Septembre</option>
+          <option value="10">Octobre</option>
+          <option value="11">Novembre</option>
+          <option value="12">Décembre</option>
+
+          {/* Ajoutez ici les options pour les mois */}
+        </select>
+        <select
+          value={selectedYear}
+          onChange={(e) => handleFilterChange("year", e.target.value)}
+        >
+          <option value="00">Toutes les années</option>
+          <option value="2024">2024</option>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          {/* Ajoutez ici les options pour les années */}
+        </select>
+        <div className="total-prize">Somme des prix : {totalPrize}</div>
+      </div>
       <table className="table">
         <thead>
           <tr>
             <th>ID</th>
             <th>Prix</th>
             <th>Date</th>
-            <th>User ID</th>
-            <th>Model ID</th>
+            <th>User</th>
+            <th>Model</th>
           </tr>
         </thead>
         <tbody>
@@ -36,7 +105,7 @@ function Accounting() {
               <td>{buy.prize}</td>
               <td>{buy.date}</td>
               <td>
-                {buy.firstname + ' '} 
+                {buy.firstname + " "}
                 {buy.lastname}
               </td>
               <td>{buy.model}</td>
